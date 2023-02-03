4f1f1a9f14ccd47d900cf76a30cf90a0939914ed
distance458 <40862498+distance458@users.noreply.github.com>
2023-02-03 15:15:28 +0300

    Create foradss.php

---
+++ foradss.php
@@ -0,0 +1,10 @@
+<!-- Google tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-QSY22XK69D">
+</script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-QSY22XK69D');
+</script>
